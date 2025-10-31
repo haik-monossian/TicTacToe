547f20bdb9877072e398f5f49edb164e6454d05a
unknown <haik.monossian@laplateforme.io>
2025-10-31 14:47:53 +0100

    Add comments

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,10 @@
 cases={1:" ", 2:" ", 3:" ", 4:" ", 5:" ", 6:" ", 7:" ", 8:" ", 9:" "}
 
 
-def ask_player_cell(player_turn):
-    if player_turn == 1:
+def ask_player_cell(player_turn): # Ask at the player in which cell he want to play
+    if player_turn == 1: # Player X
         cell = int(input("Player 1 turn (Enter a number 1 - 9) : "))
-        while True :
+        while True : # Input security 
             if cell > 9 or cell < 1:
                 cell = int(input("Retry, Player 1 turn (Enter a number 1 - 9 !!!) : "))
             elif cases[cell] != " ":
@@ -13,9 +13,9 @@ def ask_player_cell(player_turn):
                 cases[cell]="X"
                 break
 
-    elif player_turn == 2:
+    elif player_turn == 2: # Player O
         cell = int(input("Player 2 turn (Enter a number 1 - 9) : "))
-        while True:
+        while True: # Input security
             if cases[cell] != " ":
                 cell = int(input("Cell taken !!, retry, Player 2 turn (Enter a number 1 - 9) : "))
             else:
@@ -23,14 +23,14 @@ def ask_player_cell(player_turn):
                 break
 
 
-def change_player_turn(player_turn):
+def change_player_turn(player_turn): # Change the player turn 
     if player_turn == 1 :
         return 2
     elif player_turn == 2 :
         return 1
 
 
-def check_win_condition(game_is_running):
+def check_win_condition(game_is_running): # Check if there is a win combo or if it's a draw
     match cases:
         case {1:"X", 2:"X",3:"X"}:
             return False, "Player 1 Win"
@@ -59,7 +59,7 @@ def check_win_condition(game_is_running):
     else :
         return game_is_running, None
 
-def display_cells():
+def display_cells(): # Show the cells in the terminal
     print("")
     print(f" {cases[1]} | {cases[2]} | {cases[3]} ")
     print("-----------")
@@ -68,7 +68,7 @@ def display_cells():
     print(f" {cases[7]} | {cases[8]} | {cases[9]} ")
     print("")
 
-def init_game():
+def init_game(): # Game logic where we use all functions
     game_is_running = True
     player_turn = 1
 
@@ -81,4 +81,5 @@ def init_game():
         player_turn = change_player_turn(player_turn)
 
 
+#Start the game 
 init_game()
